83cf8d7876499d6f64e4c6839eafc3aa12f7ee0e
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2024-02-05 21:20:03 +0100

    loops1

---
+++ mag/01.js
@@ -0,0 +1,8 @@
+function addGeld(){
+    let geldEl = document.getElementById("m1");
+    let maasDegeri = geldEl.value;
+}
+if(maasDegeri<1000){
+   
+}
+ document.querySelector("#inpMsg").value = `selam ${inputel}`;
