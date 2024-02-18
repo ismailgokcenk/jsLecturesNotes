d7844d22fe2cf03812ee9b6764e0fb7e811e03cb
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2024-02-18 10:52:15 +0100

    da15

---
+++ day14/js/64-script.js
@@ -0,0 +1,19 @@
+import {countries} from "../data/countries.js"
+//console.log(countries)
+const loadData = () => { 
+
+    let selectOptions=""
+
+    for(let country of  countries){
+      //country.name.common
+selectOptions+=`<option> ${country.name.common} </option>`
+
+
+    }
+document.getElementById("selectID").innerHTML=selectOptions
+
+return selectOptions;
+
+ }
+
+loadData()
