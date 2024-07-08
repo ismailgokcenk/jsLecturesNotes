0beb8a5c10cee0cc94f8babb0806c5fe4d0cebeb
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2024-07-08 15:09:52 +0200

    tag35

--- .vscode/day23g/day04/js/18-script.js
+++
@@ -1,8 +0,0 @@
-let h1El=document.querySelector(".title")
-console.log(h1El)
-let h1Text=h1El.innerText
-console.log(h1Text)
-document.querySelector(".message").innerText=`message: Merhaba ${h1Text} `//1.kullanim
-/*  let messageEl=document.querySelector(".message")
- messageEl.innerText=`message: Merhaba ${h1Text} `//2.kullanim
-  */
